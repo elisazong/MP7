d7edb60e69fe434a24c6914b8c4b51a423bd35b5
zyy <zyy@10.0.0.186>
2017-12-11 21:19:38 -0600

    Add java documentation

--- MP7/src/Board.java
+++ MP7/src/Board.java
@@ -1,19 +1,39 @@
 
 public class Board {
 
+	/** number of rows
+	 * 
+	 */
 	int row;
+	/** number of columns
+	 * 
+	 */
 	int column;
-	int[][] board = new int[row][column];
+	/** a board contains the information
+	 * 
+	 */
+	int[][] board;
 	
-	Board(int r, int c) {
+	/** create a board
+	 * 
+	 * @param r	- number of rows
+	 * @param c	- number of columns
+	 */
+	public Board(int r, int c) {
 		this.row = r;
 		this.column = c;
 		this.board = new int[r][c];
 	}
 
-	void setBoard() {
+	/** filled the board with random generated alive and dead cells
+	 * 
+	 */
+	public void setBoard() {
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < column; j++) {
+				/* approximately 2/3 cells are dead, 1/3 cells are alive
+				 * 
+				 */
 				if (Math.random() * 3 < 2) {
 					board[i][j] = 0;
 				} else {
--- MP7/src/GameOfLife.java
+++ MP7/src/GameOfLife.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 
 public class GameOfLife {
 	
+	/** Created a game board with user input.
+	 * 
+	 * @return a game board with user input
+	 */
 	public static Board createBoard() {
         System.out.print("How many rows will you have? (20-50): ");
         Scanner scanner = new Scanner(System.in);
@@ -24,9 +28,22 @@ public class GameOfLife {
         return user;
 	}
 	
+	/** Count the number of life around a certain position and
+	 * 	determine that position is alive or dead
+	 * 
+	 * @param user	- the input board
+	 * @param rowPos	- row position on the board
+	 * @param colPos	- column position on the board
+	 * @return 1 if the cell is alive, 0 if the cell is dead
+	 */
 	public static int countNeighbour(Board user, int rowPos, int colPos) {
 		int count = 0;
 		int left, right, upper, lower;
+		
+		/* if the cell is on the edge, the surrounding cells will be found
+		 * on the opposite edges. 
+		 * 
+		 */
 		if (rowPos == 0) {
 			upper = user.row - 1;
 		} else {
@@ -47,6 +64,10 @@ public class GameOfLife {
 		} else {
 			right = colPos + 1;
 		}
+		
+		/*	count the number of surrounding lives
+		 * 
+		 */
 		if (user.board[upper][left] == 1) {
 			count++;
 		}
@@ -74,13 +95,27 @@ public class GameOfLife {
 		return count;
 	}
 	
+	/** refresh the board and assign the cells with the rules
+	 * 	if there are two alive cells around one certain cell, it 
+	 * 	remain its current status. if there are three alive cells,
+	 * 	the cell is alive. The other surrounding status will kill 
+	 * 	the cell.
+	 * 
+	 * @param user	- input game board
+	 */
 	public static void runGame(Board user) {
+		/*	create a copy of the current board
+		 * 
+		 */
 		Board temp = new Board(user.row, user.column);
 		for (int i = 0; i < user.row; i++) {
 			for (int j = 0; j < user.column; j++) {
 				temp.board[i][j] = user.board[i][j];
 			}
 		}
+		/* refresh the board using the precious status
+		 * 
+		 */
 		for (int i = 0; i < user.row; i++) {
 			for (int j = 0; j < user.column; j++) {
 				if (countNeighbour(temp, i, j) == 3) {
@@ -93,6 +128,12 @@ public class GameOfLife {
 		}
 	}
 	
+	/** refresh the current JPanel.
+	 * 	Red cells are alive. Orange cells are dead.
+	 * 
+	 * @param user	- the current game board
+	 * @param panel	- the panel contains the color information
+	 */
 	public static void printBoard(Board user, JPanel[][] panel) {
 		for (int i = 0; i < user.row; i++) {
 			for (int j = 0; j < user.column; j++) {
@@ -108,19 +149,33 @@ public class GameOfLife {
 	public static void main(String[] args) {
 		
 		Board user = createBoard();
+		/* create a new window to show the animation
+		 * 
+		 */
 		JFrame frame = new JFrame("Game of Life");
-		JPanel[][] panel = new JPanel[user.row][user.column];
 		
+		/* place a refreshable panel at the center of the window
+		 * The color of the panel is dark gray which will separate
+		 * the cells.
+		 * 
+		 */
 		JPanel panel1 = new JPanel();
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		frame.getContentPane().add(panel1, BorderLayout.CENTER);
 		panel1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		panel1.setBackground(Color.DARK_GRAY);
 		
+		/* create an 2d array of cells to contain the color information
+		 * 
+		 */
+		JPanel[][] panel = new JPanel[user.row][user.column];
 		JPanel panel1_1 = new JPanel();
 		frame.getContentPane().add(panel1_1, BorderLayout.CENTER);
 		panel1_1.setLayout(new GridLayout(user.row, user.column, 3, 3));
 		
+		/* initialize all the cells to white.
+		 * 
+		 */
 		for (int i = 0; i < user.row; i++) {
 			for (int j = 0; j < user.column; j++) {
 				panel[i][j] = new JPanel();
@@ -128,17 +183,20 @@ public class GameOfLife {
 				panel1_1.add(panel[i][j]);
 			}
 		}
-		
+		/* Set the size of the board and place it at the center of the screen.
+		 * 
+		 */
 		frame.setSize(user.column * 15, user.row * 15);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
-
-		
+		/* show the window.
+		 * 
+		 */
 		frame.setVisible(true);
-		
-		
+		/* start the game
+		 * 
+		 */
 		while (true) {
-			System.out.println();
 			runGame(user);
 			printBoard(user, panel);
 			try {
